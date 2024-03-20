a0bbcb7d1cb7f448372506f6840cfe0cc7ad602c
Manuel Maisano <manumaisano@gmail.com>
2024-03-20 19:24:12 +0100

    array and div idex

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 //Da lì parte un timer di 30 secondi.
 //Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//creaiamo un array vuoto
+
+const randomNumber = [];
+
+//numeri che compaiono a schermo
+
+const numberOutput = document.querySelector('.number-random');
+
+console.log(numberOutput);
+
+//variabile numeri random
+
+let casualNumber = Math.floor((Math.random() * 100) + 1);
+
+console.log(casualNumber);
